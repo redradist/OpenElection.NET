57ffe8d9b091bd84dc9bd356897d08c44b095533
redradist <redradist@gmail.com>
2019-01-21 00:03:23 +0200

    Small beatificatio of /MainActivity.cs

--- OpenElection/OpenElection.Android/MainActivity.cs
+++ OpenElection/OpenElection.Android/MainActivity.cs
@@ -9,7 +9,11 @@ using Android.OS;
 
 namespace OpenElection.Droid
 {
-    [Activity(Label = "OpenElection", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "OpenElection",
+              Icon = "@mipmap/icon",
+              Theme = "@style/MainTheme",
+              MainLauncher = true,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
